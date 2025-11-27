f2901847f4cb369704371299778b9c803123fd95
abd-abdulmubeen <abdul.mubeen10@gmail.com>
2025-11-27 12:16:34 +0530

    constraint to generate 01020304

---
+++ Q9.constraint_to_generate_01020304.sv
@@ -0,0 +1,24 @@
+class A;
+rand int a[];
+constraint c1{a.size==10;}
+constraint c2 
+{
+foreach(a[i])
+if(i%2==0)
+a[i]==0;
+else
+a[i]==(i+1)/2;
+}
+function void post_randomize();
+$display("the value of a is %p",a);
+endfunction
+endclass
+
+module tb;
+A a_h;
+initial
+begin
+a_h=new();
+a_h.randomize();
+end
+endmodule 
